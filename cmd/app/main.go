47c25b5d77e081009addb2a23b0e39f2e3f47349
SaurabPoudel <poudelsaurab20@gmail.com>
2024-04-25 07:52:12 +0545

    added chi Mux

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"gothstarter/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	router := chi.NewMux()
+	router.Get("/foo", handlers.HandleFoo)
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	slog.Info("HTTP server started", "listenAddr", listenAddr)
+	http.ListenAndServe(listenAddr, router)
 }
